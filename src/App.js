c5b9b6fa992f2af1727115b9ee8b001b4a2fe218
sarthak <sarthaksemwal69@gmail.com>
2024-05-31 06:35:27 +0530

    created a new structre

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "./Component/Home";
+import Header from "./Component/Header";
+import "./App.css";
+
+function App() {
+return (
+  <div className="app">
+  <Header />
+  <Home />
+  </div>
+);
+}
+export default App;
